fb219e2801b216606d8b58468b3a9530bf58ef50
Bernardo Lares <bernardolares@fb.com>
2021-10-27 12:34:28 -0500

    Docs: document and specify possible options #3

--- R/wallet.R
+++ R/wallet.R
@@ -32,6 +32,7 @@ wallet_coins <- function() {
 #'
 #' @inheritParams trade-parameters
 #' @inheritParams limit-5-30
+#' @param type Character. Select order type. Any of: "SPOT", "MARGIN", "FUTURES".
 #' @return A data frame.
 #' @export
 #'
