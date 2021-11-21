9f8899bfe77f2486f7c72a707ca9b3e5925743df
Andrew Collier <andrew@fathomdata.dev>
2021-11-21 06:20:47 +0000

    feat: Handle empty response in wallet_daily_snapshot()

--- R/wallet-daily-snapshot.R
+++ R/wallet-daily-snapshot.R
@@ -19,7 +19,7 @@ wallet_daily_snapshot <- function(
 ) {
   check_order_type(type)
 
-  GET(
+  snapshot <- GET(
     "/sapi/v1/accountSnapshot",
     simplifyVector = FALSE,
     security_type = "USER_DATA",
@@ -29,21 +29,28 @@ wallet_daily_snapshot <- function(
       endTime = time_to_timestamp(end_time),
       limit = limit
     )
-  )$snapshotVos %>%
-    purrr::map_dfr(~ {.x$data = list(.x$data); .x}) %>%
-    unnest_wider(data) %>%
-    clean_names() %>%
-    mutate(
-      update_time = parse_time(update_time),
-      # update_time = floor_date(update_time, unit = "days"),
-      balances = lapply(balances, function(balance) {
-        bind_rows(balance) %>%
-          mutate(
-            free = as.numeric(free),
-            locked = as.numeric(locked),
-            total = free + locked
-          )
-      })
-    ) %>%
-    rename(time = update_time)
+  )$snapshotVos
+
+  if (length(snapshot) > 0) {
+    snapshot %>%
+      purrr::map_dfr(~ {.x$data = list(.x$data); .x}) %>%
+      unnest_wider(data) %>%
+      clean_names() %>%
+      mutate(
+        update_time = parse_time(update_time),
+        # update_time = floor_date(update_time, unit = "days"),
+        balances = lapply(balances, function(balance) {
+          bind_rows(balance) %>%
+            mutate(
+              free = as.numeric(free),
+              locked = as.numeric(locked),
+              total = free + locked
+            )
+        })
+      ) %>%
+      rename(time = update_time)
+  } else {
+    log_debug("No accounts found.")
+    NULL
+  }
 }
