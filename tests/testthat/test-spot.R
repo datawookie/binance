6870bc1c00c2f81a0dba15003802346afd34b852
Andrew Collier <andrew@fathomdata.dev>
2022-07-20 05:41:42 +0100

    chore: Add tests for spot_new_order()

--- tests/testthat/test-spot.R
+++ tests/testthat/test-spot.R
@@ -3,3 +3,38 @@ test_that("spot account", {
 
   expect_equal(account$account_type, "SPOT")
 })
+
+test_that("test new spot order", {
+  expect_true(
+    spot_new_order(
+      order_type = "MARKET",
+      symbol = "ETHUSDT",
+      side = "BUY",
+      quantity = 1,
+      test = TRUE
+    )
+  )
+})
+
+test_that("MIN_NOTIONAL filter", {
+  # Quantity is too small.
+  expect_error(
+    spot_new_order(
+      order_type = "MARKET",
+      symbol = "ETHUSDT",
+      side = "BUY",
+      quantity = 0.001
+    ),
+    "Filter failure: MIN_NOTIONAL"
+  )
+  # Quantity is not too small.
+  expect_error(
+    spot_new_order(
+      order_type = "MARKET",
+      symbol = "ETHUSDT",
+      side = "BUY",
+      quantity = 1
+    ),
+    NA
+  )
+})
